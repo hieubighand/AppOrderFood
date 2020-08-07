0370bf5da864d4f8143edf52ec2ffa33c5f351a5
hieubighand <hieunt.nde19058@vtc.edu.vn>
2020-08-07 09:51:25 +0700

    hieu

--- apporderfood/src/main/java/group05/App.java
+++ apporderfood/src/main/java/group05/App.java
@@ -13,10 +13,8 @@ public class App
         System.out.println( "Hieu" );
         System.out.println( "Duong" );
         System.out.println( "Duong123" );
-        System.out.println( "Duong123456" );
-        System.out.println( "Duong12345681279827528357hieu" );
 
-
+        
 
     }
 }
