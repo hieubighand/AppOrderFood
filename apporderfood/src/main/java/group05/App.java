c325537b64e92e434f5a0b0ca13d904ecb214eb1
duong2k1 <duongnta.nde19037@vtc.edu.vn>
2020-08-07 09:32:53 +0700

    l

--- apporderfood/src/main/java/group05/App.java
+++ apporderfood/src/main/java/group05/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Wohgfdhgcjhvjhrld!" );
-        System.out.println( "Hieu" );
-        System.out.println( "Duong" );
+        System.out.println( "Hello World!" );
     }
 }
