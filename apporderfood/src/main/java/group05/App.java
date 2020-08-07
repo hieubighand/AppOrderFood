db58162a2f229a9459c3d6f1f8a36685d990e216
hieubighand <hieunt.nde19058@vtc.edu.vn>
2020-08-07 09:30:44 +0700

    name

--- apporderfood/src/main/java/group05/App.java
+++ apporderfood/src/main/java/group05/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Wohgfdhgcjhvjhrld!" );
+        System.out.println( "Hieu" );
+        System.out.println( "Duong" );
     }
 }
